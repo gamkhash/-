cb26840fb884b93d9cd83f82097f136dc6539b37
Alexander Gurin <165130106+AlexenderGurin@users.noreply.github.com>
2024-05-24 20:18:59 +0300

    Update Form1.cs
    Были добавлены новые комментарии.

--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -17,47 +17,51 @@ using System.Windows.Forms;
 
 namespace Аптека
 {
-    public partial class Form1 : Form
+public partial class Form1 : Form
+{
+public Form1()
+{
+InitializeComponent();
+}
+public class UserManager
+{
+private string connectionString = "Data Source=DESKTOP-JC5A2Q8\SQLEXPRESS;Initial Catalog= Аптека;Integrated Security=True";
+public bool AuthenticateUser(string userName, string userPassword)
+{
+using (SqlConnection connection = new SqlConnection(connectionString))
+{
+connection.Open();
+string query = "SELECT COUNT(*) FROM P_and_L WHERE Login  = @Login AND Passworsd = @Passworsd"; // Комментарий: Выборка количества записей в таблице P_and_L, где значение Login равно переданному userName и значение Passworsd равно переданному userPassword
+SqlCommand command = new SqlCommand(query, connection);
+command.Parameters.AddWithValue("@Login", userName);
+command.Parameters.AddWithValue("@Passworsd", userPassword);
+int count = (int)command.ExecuteScalar();
+return count > 0;
+}
+}
+}
+
+    private void button1_Click(object sender, EventArgs e)
     {
-        public Form1()
-        {
-            InitializeComponent();
-        }
-        public class UserManager
-        {
-            private string connectionString = "Data Source=DESKTOP-JC5A2Q8\\SQLEXPRESS;Initial Catalog= Аптека;Integrated Security=True";
-                public bool AuthenticateUser(string username, string password)
-            {
-                using (SqlConnection connection = new SqlConnection(connectionString))
-                {
-                    connection.Open();
-                    string query = "SELECT COUNT(*) FROM P_and_L WHERE Login  = @Login AND Passworsd = @Passworsd";
-                    SqlCommand command = new SqlCommand(query, connection);
-                    command.Parameters.AddWithValue("@Login", username);
-                    command.Parameters.AddWithValue("@Passworsd", password);
-                    int count = (int)command.ExecuteScalar();
-                    return count > 0;
-                }
-            }
-        }
+        string userName = TextBoxUsername.Text;
+        string userPassword = PasswordTextBox.Text;
 
-        private void button1_Click(object sender, EventArgs e)
+        UserManager userManager = new UserManager();
+        if (userManager.AuthenticateUser(userName, userPassword))
         {
-            string username = TextBoxUsername.Text;
-            string password = PasswordTextBox.Text;
-
-            UserManager userManager = new UserManager();
-            if (userManager.AuthenticateUser(username, password))
-            {
-                MessageBox.Show("Вы успешно вошли в систему");
-                Form2 form = new Form2();
-                form.Show();
-                this.Hide();
-            }
-            else
-            {
-                MessageBox.Show("Неправильное имя пользователя или пароль!");
-            }
+            MessageBox.Show("Вы успешно вошли в систему");
+            Form2 form = new Form2();
+            form.Show();
+            this.Hide();
+        }
+        else
+        {
+            MessageBox.Show("Неправильное имя пользователя или пароль!");
         }
     }
 }
+}
+
+
+
+
